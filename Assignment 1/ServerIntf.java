3c3f876b5522d2f6869305332210618896b6d299
nil2411 <142203304+nil2411@users.noreply.github.com>
2026-05-04 22:37:32 +0530

    Remove unused methods from ServerIntf
    Removed unused methods from ServerIntf interface.

--- Assignment 1/ServerIntf.java
+++ Assignment 1/ServerIntf.java
@@ -10,12 +10,5 @@ interface ServerIntf extends Remote {
 
     public int multiplication(int a, int b) throws RemoteException;
 
-    public int square(int a) throws RemoteException;
-
-    public int squareroot(int a) throws RemoteException;
-
-    public void palindrome(String str) throws RemoteException;
-
-    public void isequalstring(String str1, String str2) throws RemoteException;
-
+  
 }
